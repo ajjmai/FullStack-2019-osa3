f01228152722bb3e8b8ba5bd2e32ff154a64882e
Aija Valleala <aija.valleala@aalto.fi>
2019-04-19 11:12:08 +0300

    front-end build

---
+++ build/precache-manifest.31375364c2676c9567b80bbc853e1d7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e6ff1572f2e49f852935",
+    "url": "/static/js/main.63345bcb.chunk.js"
+  },
+  {
+    "revision": "860d068cde355ed8f94b",
+    "url": "/static/js/2.9fa6a1de.chunk.js"
+  },
+  {
+    "revision": "e6ff1572f2e49f852935",
+    "url": "/static/css/main.a6a46bfa.chunk.css"
+  },
+  {
+    "revision": "5a22dbdb4c093f02bd320c497138b2d0",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.6f1a48f3eef8c8e4a74b6cdb47bdd8b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f735bc3c6695694a4b4d",
+    "url": "/static/js/main.8ca16c66.chunk.js"
+  },
+  {
+    "revision": "860d068cde355ed8f94b",
+    "url": "/static/js/2.9fa6a1de.chunk.js"
+  },
+  {
+    "revision": "f735bc3c6695694a4b4d",
+    "url": "/static/css/main.a6a46bfa.chunk.css"
+  },
+  {
+    "revision": "051402f212c1fd1383e887eeb873b9b3",
+    "url": "/index.html"
+  }
+];
