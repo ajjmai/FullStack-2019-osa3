d75fd5dc46b9d9fdd6d4ceaa52077258344f3b13
Aija Valleala <aija.valleala@aalto.fi>
2019-04-19 10:54:14 +0300

    tehtävä 3.9

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(bodyParser.json());
 morgan.token("data", req => {
@@ -10,6 +11,7 @@ morgan.token("data", req => {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
+app.use(cors());
 
 let persons = [
   {
